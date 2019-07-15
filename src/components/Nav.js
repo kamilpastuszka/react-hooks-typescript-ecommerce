8d259be886cff538082c7f62bfa92c8b66135265
Kamil <kamil_p7@o2.pl>
2019-07-15 09:09:20 +0200

    integating react-icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaShoppingCart, FaStore } from "react-icons/fa";
 
 export default function Nav() {
   return (
@@ -6,7 +7,16 @@ export default function Nav() {
       <nav className="navbar navbar-expand-lg navbar-light bg-light align-content-between">
         <div className="navbar-brand">ecommerce app</div>
         <div className="collapse navbar-collapse" id="navbarColor02" />
-        <div className="pull-right">basket </div>
+        <div className="pull-right align-items-center">
+          <span className="mr-3">
+            {" "}
+            store <FaStore />{" "}
+          </span>
+          <span>
+            {" "}
+            basket <FaShoppingCart />{" "}
+          </span>
+        </div>
       </nav>
     </>
   );
