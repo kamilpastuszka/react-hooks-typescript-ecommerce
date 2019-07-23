b22b3b35da31b06ab735b8a017f3829ef16614e9
Kamil <kamil_p7@o2.pl>
2019-07-23 10:30:57 +0200

    changes to local state, added useEffect

--- src/containers/Container.tsx
+++ src/containers/Container.tsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Nav from "../components/Nav";
 import Basket from "../components/Basket";
 import Products from "../components/Products";
 import data from "../data/data";
+import { IState } from "../interfaces/interfaces";
 
 export default function Container() {
+  const [items, setItems] = useState<IState[]>([]);
+
+  const fetchData = () => {
+    setItems(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Router>
       <Nav />
       <Switch>
-        <Route exact path="/" component={() => <Products data={data} />} />
+        <Route exact path="/" component={() => <Products data={items} />} />
         <Route path="/basket" component={Basket} />
       </Switch>
     </Router>
