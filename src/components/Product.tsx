79bcf243e7c296ecd9633e224e495545292f6467
Kamil <kamil_p7@o2.pl>
2019-07-24 12:35:47 +0200

    style changes for Product component

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -5,17 +5,17 @@ export default function Product({ name, price, addProduct }) {
   return (
     <div className="col-sm-12 col-md-5 mt-5 justify-content-around">
       <div className="card">
-        <div className="card-body py-5">
+        <CardBody className="card-body py-5">
           <CardBodyConent>{name}</CardBodyConent>
-        </div>
-        <div className="card-footer d-flex justify-content-around">
+        </CardBody>
+        <CardFooter className="card-footer d-flex justify-content-around">
           {price}
           <div>
             <button className="btn btn-primary" onClick={addProduct}>
               add
             </button>
           </div>
-        </div>
+        </CardFooter>
       </div>
     </div>
   );
@@ -24,4 +24,12 @@ export default function Product({ name, price, addProduct }) {
 const CardBodyConent = styled.div`
   text-align: center;
   font-size: 1.5em;
+`;
+
+const CardBody = styled.div`
+  background-color: aliceblue;
+`;
+
+const CardFooter = styled.div`
+  font-size: 1.25em;
 `;
